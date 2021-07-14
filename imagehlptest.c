5a99a3e6370c86c396b287899a306e223508b409
Michael Brown <mbrown@fensystems.co.uk>
2021-07-14 12:32:35 +0100

    Allow for multiple tests per image filename
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- imagehlptest.c
+++ imagehlptest.c
@@ -3,10 +3,35 @@
 #include <windows.h>
 #include <imagehlp.h>
 
+static BOOL check_map_and_load ( const char *filename ) {
+	LOADED_IMAGE image;
+
+	/* Test ability to MapAndLoad() */
+	if ( ! MapAndLoad ( filename, NULL, &image, FALSE, TRUE ) ) {
+		fprintf ( stderr, "%s: failed MapAndLoad() with error %#x\n",
+			  filename, GetLastError() );
+		return FALSE;
+	}
+
+	/* Free resources */
+	UnMapAndLoad ( &image );
+
+	return TRUE;
+}
+
+static BOOL check_image ( const char *filename ) {
+	BOOL ok;
+
+	/* Check MapAndLoad() */
+	if ( ! check_map_and_load ( filename ) )
+		return FALSE;
+
+	return TRUE;
+}
+
 int main ( int argc, char **argv ) {
 	const char *filename;
 	BOOL ok;
-	LOADED_IMAGE image;
 	DWORD err;
 	int i;
 
@@ -20,15 +45,10 @@ int main ( int argc, char **argv ) {
 	/* Check each image */
 	for ( i = 1 ; i < argc ; i++ ) {
 		filename = argv[i];
-		printf ( "%s... ", filename );
-		ok = MapAndLoad ( filename, NULL, &image, FALSE, TRUE );
-		if ( ! ok ) {
-			err = GetLastError();
-			printf ( "failed (error %#x)\n", err );
+		ok = check_image ( filename );
+		if ( ! ok )
 			exit ( 1 );
-		}
-		printf ( "ok\n" );
-		UnMapAndLoad ( &image );
+		printf ( "%s: ok\n", filename );
 	}
 
 	return 0;
